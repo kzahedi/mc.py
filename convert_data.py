7cdef600e5b6d67768fad18d026606f58cbf46d1
kzahedi <keyan.zahedi@gmail.com>
2015-03-06 00:56:27 +0100

    modifed convert_data.py

--- convert_data.py
+++ convert_data.py
@@ -12,8 +12,3 @@ parser.add_argument("-i", type=str, default=None, help="Input file.")
 parser.add_argument("-o", type=str, default=None, help="Output file.")
 
 args = parser.parse_args()
-
-a    = get_positions(args.i)
-
-for b in a:
-  print b
